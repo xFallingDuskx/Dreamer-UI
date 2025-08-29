91c6353e372780e2f3564ad360e3efab9317d351
xFallingDuskx <nova@moondreams.dev>
2025-08-28 23:39:15 -0400

    [fbk] apply

--- lib/src/utils/downloadFile.ts
+++ lib/src/utils/downloadFile.ts
@@ -3,7 +3,7 @@
  *
  * @param content - The content to be downloaded.
  * @param filename - The name of the file to be downloaded.
- * @param type - The MIME type of the file. Defaults to 'text/plain'. Defaults to 'text/plain'.
+ * @param type - The MIME type of the file. Defaults to 'text/plain'.
  */
 export function downloadFile(content: string, filename: string, type = 'text/plain') {
   const blob = new Blob([content], { type });
