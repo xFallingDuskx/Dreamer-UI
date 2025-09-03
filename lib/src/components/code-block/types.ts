7e910b3517781c793f22df1d433daa1e4252f9dd
xFallingDuskx <nova@moondreams.dev>
2025-09-02 20:03:01 -0400

    move logic to separate files

---
+++ lib/src/components/code-block/types.ts
@@ -0,0 +1,30 @@
+export interface TSTokenClasses {
+  keyword?: string;
+  type?: string;
+  string?: string;
+  'jsx-bracket'?: string;
+  'jsx-tag'?: string;
+  'jsx-attribute'?: string;
+  'jsx-brace'?: string;
+  jsx?: string;
+  property?: string;
+  number?: string;
+  comment?: string;
+  function?: string;
+  hook?: string;
+  operator?: string;
+  plain?: string;
+}
+export type TSTokenType = keyof TSTokenClasses;
+
+export interface JsonTokenClasses {
+  key?: string;
+  string?: string;
+  number?: string;
+  boolean?: string;
+  punctuation?: string;
+  plain?: string;
+}
+export type JsonTokenType = keyof JsonTokenClasses;
+
+export type TokenClasses = TSTokenClasses | JsonTokenClasses;
