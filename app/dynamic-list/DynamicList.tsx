5db91269ed63121fbc5c44a92f64a93dd49fbab4
xFallingDuskx <nova@moondreams.dev>
2025-09-18 23:51:03 -0400

    remove leading padding when no marker

--- app/dynamic-list/DynamicList.tsx
+++ app/dynamic-list/DynamicList.tsx
@@ -286,7 +286,7 @@ export function DynamicList<T extends object>({
 
 								{/* Item Content */}
 								<div className={join('flex-1 flex min-w-0')}>
-									<div className={join('flex-1 min-w-0 py-2', !allowReorder && !marker ? 'px-3' : 'pr-3')}>
+									<div className={join('flex-1 min-w-0 py-2 pr-2')}>
 										{itemRenderFunction ? (
 											itemRenderFunction(item, originalIndex)
 										) : (
